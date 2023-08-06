dfae8ed838ec9f004f7906a8c0ea692f2cef5d4f
FlyingThaCat <FlyingThaCat@users.noreply.github.com>
2023-08-06 17:43:13 +0700

    fix client not updating

--- app/page.js
+++ app/page.js
@@ -1,64 +1,87 @@
-import './components/footer'
-import Footer from './components/footer'
-import Header from './components/header'
-import Announcement from './components/announcement'
-import QuestionBlock from './components/questionBlock'
-import AnswerBlock from './components/answerBlock'
-import {randomIp, getRandomCidr} from './datas/IPs'
-import {Netmask} from 'netmask'
-
+'use client';
+import React from 'react';
+import './components/footer';
+import Footer from './components/footer';
+import Header from './components/header';
+import Announcement from './components/announcement';
+import QuestionBlock from './components/questionBlock';
+import AnswerBlock from './components/answerBlock';
+import { randomIp, getRandomCidr } from './datas/IPs';
+import { Netmask } from 'netmask';
 
 export default function Home() {
-  const givenIp = randomIp();
-  const cidr = getRandomCidr(givenIp);
-  var block = new Netmask(`${givenIp}/${cidr}`);
-
-  const ipOctets = block.base.split('.');
-  const cidrValue = parseInt(cidr, 10);
-
-  let gateway;
-  if (cidrValue <= 8) {
-    // Class A: Only the last octet is incremented by 1
-    gateway = `${ipOctets[0]}.${ipOctets[1]}.${ipOctets[2]}.${parseInt(ipOctets[3]) + 1}`;
-  } else if (cidrValue <= 16) {
-    // Class B: The last two octets are incremented by 1
-    gateway = `${ipOctets[0]}.${ipOctets[1]}.${parseInt(ipOctets[2]) + 1}.${parseInt(ipOctets[3]) + 1}`;
-  } else if (cidrValue <= 24) {
-    // Class C: Only the last octet is incremented by 1
-    gateway = `${ipOctets[0]}.${ipOctets[1]}.${ipOctets[2]}.${parseInt(ipOctets[3]) + 1}`;
-  }
-
-  const datas = {
-    "question" : {
-      ip: givenIp,
-      cidr: cidr
+  // Initialize the datas state with the initial data
+  const [datas, setDatas] = React.useState({
+    question: {
+      ip: '',
+      cidr: 24,
     },
-    "correctAnswer" : {
-      ipNetwork: block.base,
-      subnetMask: block.mask,
-      cidr: block.bitmask,
-      wildcardMask: block.hostmask,
-      broadcast: block.broadcast,
-      totalHost: block.size,
-      ipStart: block.first,
-      ipLast: block.last,
-      ipGateway: gateway
+    correctAnswer: {
+      ipNetwork: '',
+      subnetMask: '',
+      cidr: 24,
+      wildcardMask: '',
+      broadcast: '',
+      totalHost: 256,
+      ipStart: '',
+      ipLast: '',
+      ipGateway: '',
+    },
+  });
+
+  // Function to update the datas state with new data
+  const updateDatas = () => {
+    const givenIp = randomIp();
+    const cidr = getRandomCidr(givenIp);
+    var block = new Netmask(`${givenIp}/${cidr}`);
+
+    const ipOctets = block.base.split('.');
+    const cidrValue = parseInt(cidr, 10);
+
+    let gateway;
+    if (cidrValue <= 8) {
+      gateway = `${ipOctets[0]}.${ipOctets[1]}.${ipOctets[2]}.${parseInt(ipOctets[3]) + 1}`;
+    } else if (cidrValue <= 16) {
+      gateway = `${ipOctets[0]}.${ipOctets[1]}.${parseInt(ipOctets[2]) + 1}.${parseInt(ipOctets[3]) + 1}`;
+    } else if (cidrValue <= 24) {
+      gateway = `${ipOctets[0]}.${ipOctets[1]}.${ipOctets[2]}.${parseInt(ipOctets[3]) + 1}`;
     }
-  }
+
+    setDatas({
+      question: {
+        ip: givenIp,
+        cidr: cidr,
+      },
+      correctAnswer: {
+        ipNetwork: block.base,
+        subnetMask: block.mask,
+        cidr: block.bitmask,
+        wildcardMask: block.hostmask,
+        broadcast: block.broadcast,
+        totalHost: block.size,
+        ipStart: block.first,
+        ipLast: block.last,
+        ipGateway: gateway,
+      },
+    });
+  };
+
+  // Call the updateDatas function to generate the initial data on component mount
+  React.useEffect(() => {
+    updateDatas();
+  }, []);
 
   return (
     <>
-    <Header></Header>
-    <Announcement></Announcement>
-    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
-    <main>
-      <div className="">
-      {/* <LevelSelector></LevelSelector> */}
-      <QuestionBlock datas={datas}></QuestionBlock>
-      <AnswerBlock datas={datas}></AnswerBlock>
-      </div>
-    </main>
-    <Footer></Footer>
+      <Header />
+      <Announcement />
+      <main>
+        <div className="">
+          <QuestionBlock datas={datas} />
+          <AnswerBlock datas={datas} />
+        </div>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
